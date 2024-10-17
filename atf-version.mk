808d51ca04b5b20968e5330c64482e0af9c4d5cb
sbwml <admin@cooluc.com>
2024-10-18 07:54:28 +0800

    rk3568: bl31: update to v2.8.22
    Signed-off-by: sbwml <admin@cooluc.com>

--- atf-version.mk
+++ atf-version.mk
@@ -6,7 +6,7 @@ RK3399_ATF:=rk3399_bl31_v1.36.elf
 RK3399_DDR:=rk3399_ddr_800MHz_v1.30.bin
 RK3399_LOADER:=rk3399_miniloader_v1.30.bin
 
-RK3568_ATF:=rk3568_bl31_v1.44.elf
+RK3568_ATF:=rk3568_bl31_v2.8.22.elf
 RK3568_DDR:=rk3568_ddr_1560MHz_v1.21.bin
 
 RK3588_ATF:=rk3588_bl31_v1.45.elf
