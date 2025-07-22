e3f5f9ee607554fa563f49ad2ac61f25025a51ee
sbwml <admin@cooluc.com>
2025-07-22 17:55:37 +0800

    rkbin: use mainline ATF for nanopi-r76s
    Signed-off-by: sbwml <admin@cooluc.com>

--- atf-version.mk
+++ atf-version.mk
@@ -9,8 +9,8 @@ RK3399_LOADER:=rk3399_miniloader_v1.30.bin
 RK3568_ATF:=rk3568_bl31_v1.44.elf
 RK3568_DDR:=rk3568_ddr_1560MHz_v1.23.bin
 
-RK3576_ATF:=rk3576_bl31_v1.15.elf
-RK3576_DDR:=rk3576_ddr_lp4_2112MHz_lp5_2736MHz_v1.09.bin
+RK3576_ATF:=rk3576_bl31_v2.13.0.elf
+RK3576_DDR:=rk3576_ddr_lp4_1866MHz_lp5_2736MHz_v1.09.bin
 
 RK3588_ATF:=rk3588_bl31_v1.48.elf
 RK3588_DDR:=rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.18.bin
